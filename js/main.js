f2c8f6f950400cb3fa0ae81973fb140bda6b77da
mokapadia <120064732+mokapadia@users.noreply.github.com>
2023-04-04 22:23:17 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // create frame constants
 const FRAME_HEIGHT = 800;
-const FRAME_WIDTH = 800; 
+const FRAME_WIDTH = 1000; 
 const MARGINS = {left: 150, right: 50, top: 50, bottom: 50};
 
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
@@ -26,14 +26,16 @@ const FRAME2 = d3.select("#vis2")
 
   // create projection to plot longititude/latitude 
    let projection = d3.geoMercator().center([-72.02, 42])
-                    .scale(10000)
+                    .scale(12000)
                     .translate([VIS_WIDTH/2,VIS_HEIGHT/2]);
+
+   let g = FRAME2.append("g");
 
    
 // plot geoJSON of mass
 d3.json("data/usa.json").then((data) => { 
       
-        FRAME2.append("g").selectAll(
+        g.selectAll(
                    "path").data(data.features).enter().append(
                    "path").attr("fill", "white").attr(
                    "d", d3.geoPath().projection(projection)).style(
@@ -57,11 +59,13 @@ d3.csv("data/food_retailers.csv").then((data) => {
 
   // bar chart based on establishment type
   // caulctae number of entries per establishment type
-  const count = d3.rollup(data, g => g.length, d => d.prim_type);
+  let count = d3.rollup(data, g => g.length, d => d.prim_type);
 
   // determine max numnber
-  const nums = count.values();
-  const MAX_AMT = d3.max(nums);
+  let nums = count.values();
+  let MAX_AMT = d3.max(nums);
+
+
 
     // create scale  for x scale 
   const AMT_SCALE = d3.scaleLinear() 
@@ -86,6 +90,9 @@ d3.csv("data/food_retailers.csv").then((data) => {
           .attr("height", CATEGORY_SCALE.bandwidth())
           .style("fill", (d) => {return color(d[0]); })
           .attr("class", "bar");
+
+
+    
 
       // create new variable for tooltip
     const TOOLTIP = d3.select("#vis1")
@@ -137,7 +144,12 @@ d3.csv("data/food_retailers.csv").then((data) => {
         .attr("transform", "translate(" + MARGINS.left + 
               "," + (MARGINS.top) + ")") 
         .call(d3.axisLeft(CATEGORY_SCALE))
-          .attr("font-size", '10px');
+          .attr("font-size", '10px')
+        .append('text')
+        .attr('class', 'axis-label')
+        .text("Type of Establishment")
+        .attr('x', MARGINS.left + (VIS_WIDTH) / 2)
+        .attr('y', 50);
     
 
     // append x axis
@@ -145,32 +157,42 @@ d3.csv("data/food_retailers.csv").then((data) => {
         .attr("transform", "translate(" + (MARGINS.left) + 
               "," + (MARGINS.bottom +VIS_HEIGHT) + ")") 
         .call(d3.axisBottom(AMT_SCALE).ticks(10)) 
-          .attr("font-size", '20px');
+          .attr("font-size", '20px')
+        .append('text')
+        .attr('class', 'axis-label')
+        .text("Type of Establishment")
+        .attr('x', MARGINS.left + (VIS_WIDTH) / 2)
+        .attr('y', MARGINS.bottom +VIS_HEIGHT);
 
 
 
     //SECOND VIS - scatter plot
     
-    // set zoom for vis2 that calles updateChart
-      let zoom = d3.zoom().on('zoom', handleZoom).scaleExtent([.5, 10]).extent([[0, 0], [VIS_WIDTH, VIS_HEIGHT]]);
-      function handleZoom(event) {
-          FRAME2.attr('transform', event.transform);
-      };
-      
+        let circleR = 5;
     
       // Plots the data points on to the scatter plot 
-    FRAME2.selectAll("points")
+    g.selectAll("points")
           .data(data)
           .enter()
           .append("circle")
           .attr("cx", (d) => { return projection([d.longitude,d.latitude])[0]; })
           .attr("cy", (d) => { return projection([d.longitude,d.latitude])[1]; })
-          .attr("r", 3)
+          .attr("r", circleR)
           .attr("class", "point")
-          .style("fill", (d) => {return color(d.prim_type); })
-          .call(zoom)
-          .append("g");
+          .style("fill", (d) => {return color(d.prim_type); });
 
+
+    // set zoom for vis2 that calls handleZoom
+      let zoom = d3.zoom().on('zoom', handleZoom).scaleExtent([1, 10]).extent([[0, 0], [VIS_WIDTH, VIS_HEIGHT]]);
+     
+      function handleZoom({transform}) {
+          g.attr('transform', transform);
+          console.log(transform.k);
+          g.selectAll("circle").attr('r', circleR / transform.k);
+      };
+      
+
+    FRAME2.call(zoom);
     
 
     // create new variable for tooltip
@@ -216,11 +238,130 @@ d3.csv("data/food_retailers.csv").then((data) => {
     };
 
 
-  // Add event listeners
+  
+
+
+  let allGroup = d3.group(data, (d) => d.municipal).keys();
+  let groupArray = Array.from(allGroup);
+
+  
+    // add the options to the button
+  
+      let options = '';
+
+        for (let i = 0; i < groupArray.length; i++) {
+          options += '<option value="' + groupArray[i] + '">';
+        };
+
+     
+
+        document.getElementById('muncipals').innerHTML = options;
+
+
+    // // A function that update the chart
+     function update() {
+
+      let selectedGroup = document.getElementById("mun");
+      let str  = String(selectedGroup.value);
+
+  
+    //   // Create new data with the selection?
+       let dataFilter = data.filter((d) => {return d.municipal== str})
+       console.log(dataFilter);
+
+       g.selectAll("circle").remove();
+
+     // Give these new data to update line
+         g.selectAll("points")
+            .data(dataFilter)
+            .enter()
+            .append("circle")
+            .attr("cx", (d) => { return projection([d.longitude,d.latitude])[0]; })
+            .attr("cy", (d) => { return projection([d.longitude,d.latitude])[1]; })
+            .attr("r", 3)
+            .attr("class", "point")
+            .style("fill", (d) => {return color(d.prim_type); })
+            
+          FRAME2.call(zoom);
+
+           // Add event listeners
     FRAME2.selectAll(".point")
           .on("mouseover", handleMouseover2) 
           .on("mousemove", handleMousemove2)
           .on("mouseleave", handleMouseleave2);    
+
+
+          // caulctae number of entries per establishment type
+    let count = d3.rollup(dataFilter, g => g.length, d => d.prim_type);
+
+    // determine max numnber
+    let nums = count.values();
+    let MAX_AMT = d3.max(nums);
+
+      // create scale  for x scale 
+    const AMT_SCALE = d3.scaleLinear() 
+                        .domain([0, MAX_AMT + MAX_AMT/10]) 
+                        .range([0, VIS_WIDTH]); 
+
+    // create y axis scale based on category names
+      const CATEGORY_SCALE = d3.scaleBand() 
+                  .domain(dataFilter.map((d) => { return d.prim_type; })) 
+                  .range([0, VIS_HEIGHT])
+                  .padding(.2); 
+
+
+    FRAME1.selectAll("rect").remove();
+      // plot bar based on data with rectangle svgs 
+    FRAME1.selectAll("bar")  
+          .data(count) 
+          .enter()  
+          .append("rect")  
+            .attr("x", MARGINS.left) 
+            .attr("y", (d) => { return CATEGORY_SCALE(d[0]) + MARGINS.bottom;}) 
+            .attr("width", (d) => { return AMT_SCALE(d[1]); })
+            .attr("height", CATEGORY_SCALE.bandwidth())
+            .style("fill", (d) => {return color(d[0]); })
+            .attr("class", "bar")
+            .call(zoom)
+            .append("g");
+
+        FRAME1.selectAll("g").remove();
+
+       // append y axis 
+         FRAME1.append("g") 
+              .attr("transform", "translate(" + MARGINS.left + 
+                    "," + (MARGINS.top) + ")") 
+              .call(d3.axisLeft(CATEGORY_SCALE))
+                .attr("font-size", '10px');
+
+        // append x axis
+    FRAME1.append("g") 
+        .attr("transform", "translate(" + (MARGINS.left) + 
+              "," + (MARGINS.bottom +VIS_HEIGHT) + ")") 
+        .call(d3.axisBottom(AMT_SCALE).ticks(10)) 
+          .attr("font-size", '20px');
+      
+      // Add event listeners
+      FRAME1.selectAll(".bar")
+            .on("mouseover", handleMouseover) //add event listeners
+            .on("mousemove", handleMousemove)
+            .on("mouseleave", handleMouseleave);    
+
+
+    };
+
+
+
+     // add Event Listener to button to submit new coordinates 
+    document.getElementById("submitButton").addEventListener("click", update);
+
+    // Add event listeners
+    FRAME2.selectAll(".point")
+          .on("mouseover", handleMouseover2) 
+          .on("mousemove", handleMousemove2)
+          .on("mouseleave", handleMouseleave2);    
+
+   
 
      
         
